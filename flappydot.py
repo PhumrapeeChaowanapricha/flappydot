90654704496d3d604c56f44378f67f607989511e
chayayot123 <chayayot123@hotmail.com>
2021-03-25 11:20:58 +0700

    Resolve#03

--- flappydot.py
+++ flappydot.py
@@ -8,7 +8,8 @@ CANVAS_HEIGHT = 500
 UPDATE_DELAY = 33
 GRAVITY = 2.5
 STARTING_VELOCITY = -30
-PILLAR_SPEED = 10
+PILLAR_SPEED = 8
+PILLAR_VELOCITY = -10
 
 class Dot(Sprite):
     def init_element(self):
@@ -22,9 +23,8 @@ class Dot(Sprite):
 class FlappyGame(GameApp):
     def create_sprites(self):
         self.dot = Dot(self, 'images/dot.png', CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2)
-
-        self.elements.append(self.dot)
         self.pillar_pair = PillarPair(self, 'images/pillar-pair.png', CANVAS_WIDTH, CANVAS_HEIGHT // 2)
+        self.elements.append(self.dot)
         self.elements.append(self.pillar_pair)
 
     def init_game(self):
@@ -41,7 +41,9 @@ class FlappyGame(GameApp):
 
 class PillarPair(Sprite):
     def update(self):
-        self.x += PILLAR_SPEED
+        self.vx = PILLAR_VELOCITY
+        self.x += self.vx
+        self.vx += PILLAR_SPEED
 
 
 if __name__ == "__main__":
